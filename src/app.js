880dbbcd9ff53ca6bb35836edc11b3feb2c7e328
Robert Kogami <robert@kogami.dev>
2020-05-14 10:43:51 -0400

    created router paths

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>This is from my dashboard component.</div>
+);
+
+const AddExpensePage = () => <div>This is from my AddExpense component.</div>;
+
+const EditExpensePage = () => <div>This is from my EditExpense component.</div>;
+
+const HelpPage = () => <div>This is my help page.</div>;
+
+const routes = (
+  <Router>
+    <div>
+      <Route path='/' component={ExpenseDashboardPage} exact={true} />
+      <Route path='/create' component={AddExpensePage} />
+      <Route path='/edit' component={EditExpensePage} />
+      <Route path='/help' component={HelpPage} />
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
